305b7f2df947e608cf8fd5c6e6cfa4c92218b72d
sahuno <ekwame001@gmail.com>
2019-11-13 22:30:59 -0500

    My first R scripts to try version control in Rstudio with github! tadaaa! I'm preparing to go home now, its past 10:30pm

---
+++ Rscript_test2.R
@@ -0,0 +1,1 @@
+print("Testing R studio github")
---
+++ Rscript_testGit.R
@@ -0,0 +1,4 @@
+print("This is the new repository i created to be able to try version control with github")
+print("This repositor was created in R studio")
+paste0("Name:","Samuel Ahuno", sep="_")
+paste0(c("Date", "November 13th 2019"), sep=" ")
