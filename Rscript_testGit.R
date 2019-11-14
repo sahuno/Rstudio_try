c4401bcf5e663cc9a1873c5803a46b1aca0b50a3
sahuno <ekwame001@gmail.com>
2019-11-14 13:27:51 -0500

    exploring in built data

--- Rscript_testGit.R
+++ Rscript_testGit.R
@@ -2,3 +2,6 @@ print("This is the new repository i created to be able to try version control wi
 print("This repositor was created in R studio")
 paste0("Name:","Samuel Ahuno", sep="_")
 paste0(c("Date", "November 13th 2019"), sep=" ")
+
+summary(cars)
+View(cars)
